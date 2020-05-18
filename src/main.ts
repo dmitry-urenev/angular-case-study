2b06010c2817dd40f6c3a28774c053056ea0967e
Dmitry Urenev <dmitry.urenev@gmail.com>
2020-05-18 12:45:05 +0200

    browser platform

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import './polyfills';
-
+import '@angular/compiler';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
+
 import { AppModule } from './app/app.module';
 
+platformBrowser().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, { preserveWhitespaces: false })
-  // tslint:disable-next-line:no-console - there's not much we can do here otherwise
-  .catch(e => console.error(e));
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
